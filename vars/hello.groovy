b398e38061edc558104463d60783c01e7e87af7f
Sam Manickaraj <49973507+sammanickaraj@users.noreply.github.com>
2021-01-01 13:28:30 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,8 +1,7 @@
 def call(Map map = [:]){
-    println map.name
-    println map.age
-}
-stage("Image Creation") {
-    println map.name
-    println map.age
+  
+    stage("Image Creation") {
+        println map.name
+        println map.age
+    }
 }
