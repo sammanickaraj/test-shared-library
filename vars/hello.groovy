2f006b385bb7b7775615b29bf11bbd2de69c1d3e
Sam Manickaraj <49973507+sammanickaraj@users.noreply.github.com>
2021-01-01 14:54:01 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -3,7 +3,7 @@ def call(Map map = [:]){
         println map.name
         println map.age
         println map.city
-        echo "${name}"
-        echo "${map.name}"
+        echo name
+        echo map.name
     }
   }
